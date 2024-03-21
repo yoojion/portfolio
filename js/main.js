4f3fe87562a93d571aad64502e5c8dafb71b5971
yoojion <wldnjsdl886@naver.com>
2024-03-21 11:52:56 +0900

    skill 수정

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const circles = document.querySelectorAll(".circular-pbar");
-//gsap.toArray()
 
 circles.forEach((el) => {
   const counter = el.querySelector(".circular-pbar-counter");
   const tg = counter.textContent + "%";
-  //   counter.textContent = tg;
   const tm = gsap.timeline({
-    defaults: { duration: 4, ease: "expo.out" },
+    defaults: { duration: 3, ease: "expo.out" },
     scrollTrigger: {
       trigger: el,
       toggleActions: "play pause resume reset",
@@ -14,7 +12,6 @@ circles.forEach((el) => {
   });
   tm.from(counter, {
     textContent: 0,
-    // modifiers:사용자 정의 함수
     modifiers: {
       textContent: (textContent) => {
         return textContent.toFixed();
